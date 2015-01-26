171946fa1835d128df4a25a228d6abf1305bb434
Cezary Wojtkowski <cezary@gmail.com>
2015-01-25 22:31:31 -0800

    add build scripts, add gitignore

---
+++ webpack.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  entry: './lib/json-forms.js',
+  output: {
+    filename: './dist/json-forms.js',
+    sourceMapFilename: './dist/json-forms.map',
+    library: 'JSONForms',
+    libraryTarget: 'umd'
+  }
+};
